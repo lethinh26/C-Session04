97860c2c90537c83760bce1a636df46c31dbc45b
lethinh <68421373+lethinh26@users.noreply.github.com>
2024-12-16 21:14:32 +0700

    Update Session04BT4.c

--- Session04BT4.c
+++ Session04BT4.c
@@ -3,10 +3,12 @@ int main() {
 	int month;
 	printf("Nhap vao so thang: ");
 	scanf("%d", &month);
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11) {
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
 		printf("Thang %d co 31 ngay", month);
-	}else if (month == 2 || month == 4 || month == 6 || month == 8 || month == 10 || month == 12){
+	}else if (month == 4 || month == 6 || month == 9 || month == 11){
 		printf("Thang %d co 30 ngay", month);
+	}else if (month == 2){
+		printf("Thang %d co 28 ngay", month);
 	}else {
 		printf("Thang ko hop le");
 	}
