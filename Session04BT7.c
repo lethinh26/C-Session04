1eef4b7fee792715a1ac97d12d76d1a86f863fa0
lethinh <68421373+lethinh26@users.noreply.github.com>
2024-12-16 19:33:24 +0700

    Update Session04BT7.c

--- Session04BT7.c
+++ Session04BT7.c
@@ -3,7 +3,7 @@ int main() {
 	int year;
 	printf("Nhap so nam: ");
 	scanf("%d", &year);
-	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
 		printf("%d la nam nhuan", year);
 	}else {
 		printf("%d ko phai la nam nhuan", year);
